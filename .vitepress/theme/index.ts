cacc8388fe97be933b5fc67f595973db942d2c0e
KUN1007 <yuyuyukunkunkun@gmail.com>
2024-03-21 17:24:06 +0800

    feat: custom image

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -4,6 +4,8 @@ import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
 
+import KunImage from '../components/kun-image/KunImage.vue'
+
 export default {
   extends: DefaultTheme,
   Layout: () => {
@@ -12,6 +14,6 @@ export default {
     })
   },
   enhanceApp({ app, router, siteData }) {
-    // ...
+    app.component('KunImage', KunImage)
   },
 } satisfies Theme
