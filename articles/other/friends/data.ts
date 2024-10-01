df0ec94dd2610fa1698ce6ac8b364789c1cf6973
SaltedFishes <main@saltedfishes.com>
2024-10-01 20:40:43 +0800

    ?

--- articles/other/friends/data.ts
+++ articles/other/friends/data.ts
@@ -54,12 +54,6 @@ export const FRIENDS_DATA: FriendsData[] = [
         link: 'https://snowmiku-home.top'
       },
       {
-        icon: 'https://saltedfishes.com/images/avatar.webp',
-        title: 'SaltedFishes\'s Blog',
-        desc: '「尽己所能见证一切有趣之事」',
-        link: 'https://saltedfishes.com/'
-      },
-      {
         icon: 'https://r2.toshiki.dev/image/toshiki-home-nuxt/06b8cbad3dc799e4bfe42c5d8bad2498.svg',
         title: 'Toshiki\'s Homepage',
         desc: 'Former full time INTP & current INTJ  & part time ISTP whom is caffein overdosed hyperboosted by Monster meanwhile an egoistic capitalist that overthinks ;)',
